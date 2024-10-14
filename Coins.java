398d53978825ec3b4be19287b591e85748012445
TheAppleMk12 <abramhtran@gmail.com>
2024-10-14 11:18:55 -0700

    Completed

--- Coins.java
+++ Coins.java
@@ -2,9 +2,24 @@ public class Coins
 {
   public static void main(String[] args)
   {
-    // test code here
+    int cents = 1234;
+    coinConverter(cents);
   }
 
-  // write code for coinConverter
-
+    static void coinConverter(int n)
+    {
+      int b = n/100;
+      n = n % 100;
+      System.out.println("Dollar Bills: " + b);
+      b = n/25;
+      n = n % 25;
+      System.out.println("Quarters: " + b);
+      b = n/10;
+      n = n % 10;
+      System.out.println("Dimes: " + b);
+      b = n/5;
+      n = n % 5;
+      System.out.println("Nickels: " + b);
+      System.out.println("Pennies: " + n);
+    }
 }
